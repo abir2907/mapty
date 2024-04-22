efebe90d77c3bfe934f21b2296710f34b0a282ad
Abir Chodha <abirchodha1@gmail.com>
2024-04-23 02:46:36 +0530

    Add html li of running, cycling

--- script.js
+++ script.js
@@ -187,7 +187,7 @@ class App {
   }
 
   _renderWorkout(workout) {
-    const html = `<li class="workout workout--${workout.type}" data-id="${
+    let html = `<li class="workout workout--${workout.type}" data-id="${
       workout.id
     }">
     <h2 class="workout__title">${workout.description}</h2>
@@ -203,6 +203,36 @@ class App {
       <span class="workout__value">${workout.duration}</span>
       <span class="workout__unit">min</span>
     </div>`;
+
+    if (workout.type === 'running')
+      html += `
+       <div class="workout__details">
+         <span class="workout__icon">⚡️</span>
+         <span class="workout__value">${workout.pace.toFixed(1)}</span>
+         <span class="workout__unit">min/km</span>
+       </div>
+       <div class="workout__details">
+         <span class="workout__icon">🦶🏼</span>
+         <span class="workout__value">${workout.cadence}</span>
+         <span class="workout__unit">spm</span>
+       </div>
+     </li>
+     `;
+
+    if (workout.type === 'cycling')
+      html += `
+       <div class="workout__details">
+        <span class="workout__icon">⚡️</span>
+        <span class="workout__value">${workout.speed.toFixed(1)}</span>
+        <span class="workout__unit">km/h</span>
+      </div>
+      <div class="workout__details">
+        <span class="workout__icon">⛰</span>
+        <span class="workout__value">${workout.elevationGain}</span>
+        <span class="workout__unit">m</span>
+      </div>
+     </li>
+     `;
   }
 }
 
