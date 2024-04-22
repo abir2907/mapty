14218237f776bf12debc155cb777b634b37cd5b0
Abir Chodha <abirchodha1@gmail.com>
2024-04-23 03:51:21 +0530

    Preserve previous markers with local storage We can render the workout markers only after we get the user location and load the map, so we did this render marker inside _loadMap()

--- script.js
+++ script.js
@@ -112,6 +112,8 @@ class App {
 
     // Handling clicks on map
     this.#map.on('click', this._showForm.bind(this));
+
+    this.#workouts.forEach(work => this._renderWorkoutMarker(work));
   }
 
   _showForm(mapE) {
